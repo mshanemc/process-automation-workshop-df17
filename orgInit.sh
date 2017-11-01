ef78d5b1f9fcd162e4899cff01b093145c21af41
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-11-01 17:53:13 -0500

    save after permset

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sfdx force:org:create -f config/project-scratch-def.json -s
 sfdx force:source:push
+sfdx force:user:permset:assign -n WorkshopPerms
 sfdx force:apex:execute -f apexScripts/setup.cls
 sfdx force:org:open
 
