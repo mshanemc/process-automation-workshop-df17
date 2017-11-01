bc1a86d9be06b97ff08f09512d522e41b8716b21
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-11-01 18:44:05 -0500

    1 day

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sfdx force:org:create -f config/project-scratch-def.json -s
+sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:source:push
 sfdx force:user:permset:assign -n WorkshopPerms
 sfdx force:apex:execute -f apexScripts/setup.cls
